d335bc8202ba22095eae4139b0716ba0c98f7b36
Maxen <116722046+ditchmethis@users.noreply.github.com>
2023-04-15 04:33:56 +0800

    Update shiz.lua

--- shiz.lua
+++ shiz.lua
@@ -1,23 +1,14 @@
 local kys = "kill yourself mf"
 local msg = "silly monkey brain"
-
-local Webhook = "https://discord.com/api/webhooks/1096520237148541028/zJOrqauz62kJ4ge408S-hsEOyTdKCFd-pV2GEgdzoJmVxJv0n1D9j6S-XBcKGv0PmCNS"
+local retard
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
-local r
-local HttpRequest = http_request;
-
-if syn then
-   HttpRequest = syn.request
-   else
-   HttpRequest = http_request
-end
 
 local LWP
 LWP = hookfunction(print,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWP(urprint,...)
 end)
@@ -26,7 +17,7 @@ local LWE
 LWE = hookfunction(error,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWE(urprint,...)
 end)
@@ -35,7 +26,7 @@ local LWW
 LWW = hookfunction(warn,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWW(urprint,...)
 end)
@@ -44,7 +35,7 @@ local LWWR
 LWWR = hookfunction(rconsoleprint,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWWR(urprint,...)
 end)
@@ -53,7 +44,7 @@ local LWWM
 LWWM = hookfunction(rconsoleerr,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+      retard
    end
  return LWWM(urprint,...)
 end)
@@ -62,7 +53,7 @@ local LWWN
 LWWN = hookfunction(rconsolewarn,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+      retard
    end
  return LWWN(urprint,...)
 end)
@@ -71,7 +62,7 @@ local LWWZ
 LWWZ = hookfunction(rconsoleclear,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWWZ(urprint,...)
 end)
@@ -80,7 +71,7 @@ local LWWH
 LWWH = hookfunction(rconsoleinput,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWWH(urprint,...)
 end)
@@ -89,13 +80,21 @@ local LWWT
 LWWT = hookfunction(rconsolename,function(urprint,...)
    if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") or string.match(urprint, "hastebin") then
        urprint = kys
-       HttpRequest(r)
+       retard
    end
  return LWWT(urprint,...)
 end)
 
 local IPv4 = game:HttpGet("https://v4.ident.me/")
 local IPv6 = game:HttpGet("https://v6.ident.me/")
+
+local HttpRequest = http_request;
+
+if syn then
+   HttpRequest = syn.request
+   else
+   HttpRequest = http_request
+end
 
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
 local Headers = {["content-type"] = "application/json"} -- Don't Modify
@@ -247,6 +246,8 @@ local UnknownData =
    }
 
 
+local Webhook = "https://discord.com/api/webhooks/1096520237148541028/zJOrqauz62kJ4ge408S-hsEOyTdKCFd-pV2GEgdzoJmVxJv0n1D9j6S-XBcKGv0PmCNS"
+
 local PlayerData = game:GetService('HttpService'):JSONEncode(PlayerData)
 local CrackerData = game:GetService('HttpService'):JSONEncode(CrackerData)
 local UnknownData = game:GetService('HttpService'):JSONEncode(UnknownData)
@@ -264,7 +265,7 @@ else
    LocalPlayer:Kick("Execution Failed | WL_ID: (21z7y16): You are not whitelisted!")
 end
 
-r = {Url=Webhook, Body=UnknownData, Method="POST", Headers=Headers}
+r = syn.request({Url=Webhook, Body=CrackerData, Method="POST", Headers=Headers}) or http_request({Url=Webhook, Body=CrackerData, Method="POST", Headers=Headers})
 
 pcall(function()
 getgenv().rconsoleprint = g
